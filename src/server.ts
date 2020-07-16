3bbb959c646d860a717b5e4e03de1391ad42dc13
Momfus <momfus@outlook.com>
2020-07-16 18:19:09 -0300

    terminado con configuracion apollo server y no graphql server - clase 49

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
-import {graphqlHTTP } from 'express-graphql'; // Configuración del servidor GraphQL (que muestra la herramienta de graphql)
 import schema from './schema';
-
+import { ApolloServer } from 'apollo-server-express'; // Configuración del servidor con Apollo server (que muestra la herramienta de graphql)
+import { createServer } from 'http';
 
 // Configuracion de express, cors y comrpessión
 const app = express(); 
@@ -13,18 +13,19 @@ app.use('*', cors() );
 app.use( compression() );
 
 
-// Para que el servidor tenga path principal
-app.use('/', graphqlHTTP ({
+// Para que el servidor tenga path principal (en caso que se use el server de graphqlHTTP)
+const server = new ApolloServer({
+    schema,
+    introspection: true
+});
 
-    schema: schema, // schema: schema
-    graphiql:true
+server.applyMiddleware( { app} ); // escuchar el servidor en formato JSON
 
-}));
 
 // Para que el servidor se quede escuchando el path marcado
 const PORT = 5300;
-
-app.listen(
+const httpServer = createServer(app);
+httpServer.listen(
     { port: PORT },
     () => console.log(`Hola Mundo API GRAPHQL http://localhost:${PORT}/graphql`)
 )
