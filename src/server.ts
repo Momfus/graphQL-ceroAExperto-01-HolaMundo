2bce5d22f0bc084811dfa6363a1e5356df0222b2
Momfus <momfus@outlook.com>
2020-07-16 17:22:01 -0300

    Agregado configuracion de server y puerto - clase 45

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log( 'Hola mundo!!!' );
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+
+
+// Configuracion de express, cors y comrpessión
+const app = express(); 
+
+app.use('*', cors() );
+
+app.use( compression() );
+
+// Para que el servidor tenga path principal
+app.use('/', (re, res) => {
+    res.send('Bienvenido al curso de GraphQL')
+})
+
+// Para que el servidor se quede escuchando el path marcado
+const PORT = 5300;
+
+app.listen(
+    { port: PORT },
+    () => console.log(`Hola Mundo API GRAPHQL http://localhost:${PORT}`)
+)
